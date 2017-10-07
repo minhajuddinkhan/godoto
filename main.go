406a97567ba71aa2cbb99216cf46e06266733ac7
minhajkhan75 <minhajuddinkhan75@gmail.com>
2017-10-07 12:30:29 +0500

    serve static files :smile:

--- main.go
+++ main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"github.com/minhajuddinkhan/godoto/web"
 )
 
 func main() {
 
-	r := mux.NewRouter()
-	r = web.Up(*r)
+	web.Up()
 
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":3000", nil)
 }
--- web/server.go
+++ web/server.go
@@ -1,6 +1,8 @@
 package web
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/minhajuddinkhan/godoto/controllers"
 )
@@ -9,10 +11,12 @@ var (
 	heroCtrl = controllers.HeroController{}
 )
 
-func Up(r mux.Router) *mux.Router {
+func Up() {
 
+	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/heroes", heroCtrl.InsertHero()).Methods("POST")
 	r.HandleFunc("/heroes", heroCtrl.FindAndDumpHeroes()).Methods("PUT")
 	r.HandleFunc("/heroes", heroCtrl.GetAllHeroes()).Methods("GET")
-	return &r
+	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./static/"))))
+	http.Handle("/", r)
 }
